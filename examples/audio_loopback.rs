7dbbb3136dd9ffa06315b9f173cc4b94fa2e03c9
Mike Hilgendorf <mike@hilgendorf.audio>
2026-05-06 22:35:29 -0500

    initial commit

---
+++ examples/audio_loopback.rs
@@ -0,0 +1,92 @@
+use bip_buffer_channel::channel;
+use cpal::{
+    BufferSize, InputCallbackInfo, OutputCallbackInfo, StreamConfig, StreamError,
+    traits::{DeviceTrait, HostTrait},
+};
+
+// Demo showing the synchronization of two i/o device callbacks with different buffer sizes through one channel.
+fn main() {
+    let host = cpal::default_host();
+
+    // Input device and config.
+    let input_device = host.default_input_device().unwrap();
+    let input_config = input_device.default_input_config().unwrap().into();
+    let input_config = StreamConfig {
+        buffer_size: BufferSize::Fixed(513),
+        ..input_config
+    };
+
+    // Output device and config.
+    let output_device = host.default_output_device().unwrap();
+    let output_config = output_device.default_output_config().unwrap().into();
+    let output_config = StreamConfig {
+        buffer_size: BufferSize::Fixed(499),
+        ..output_config
+    };
+
+    assert_eq!(input_config.channels, output_config.channels);
+    assert_eq!(input_config.sample_rate, output_config.sample_rate);
+
+    // Create the channel for synchronization.
+    let (mut sender, mut receiver) = channel(2 << 14, || 0.0);
+
+    // Input callback copies to the channel.
+    let input_callback = move |mut data: &[f32], _info: &InputCallbackInfo| {
+        for _ in 0..2 {
+            // If this returns an error it means the output callback dropped.
+            let Ok(mut guard) = sender.try_send() else {
+                return;
+            };
+
+            // Copy to the channel.
+            let length = guard.len().min(data.len());
+            guard[0..length].copy_from_slice(&data[0..length]);
+
+            // Commit the data.
+            guard.truncate(length);
+            guard.commit();
+
+            // Update data and bail if needed.
+            data = &data[length..];
+            if data.is_empty() {
+                break;
+            }
+        }
+    };
+
+    // Output callback copies out of the channel.
+    let output_callback = move |mut data: &mut [f32], _info: &OutputCallbackInfo| {
+        data.fill(0.0);
+        for _ in 0..2 {
+            // If this returns an error it means the input callback dropped.
+            let Ok(mut guard) = receiver.try_recv() else {
+                return;
+            };
+
+            // Copy out of the channel.
+            let length = guard.len().min(data.len());
+            data[0..length].copy_from_slice(&guard[0..length]);
+
+            // Decommit the data.
+            guard.truncate(length);
+            guard.decommit();
+
+            // Update data and bail if needed.
+            data = &mut data[length..];
+            if data.is_empty() {
+                break;
+            }
+        }
+    };
+
+    let _input_stream = input_device
+        .build_input_stream(&input_config, input_callback, error_callback, None)
+        .unwrap();
+    let _output_stream = output_device
+        .build_output_stream(&output_config, output_callback, error_callback, None)
+        .unwrap();
+
+    std::thread::sleep(std::time::Duration::from_secs(5));
+}
+
+fn error_callback(_error: StreamError) {}
---
+++ src/lib.rs
@@ -0,0 +1,184 @@
+use std::{
+    cell::UnsafeCell,
+    ops::{Deref, DerefMut, Range},
+    sync::{
+        Arc,
+        atomic::{AtomicUsize, Ordering},
+    },
+};
+
+pub struct Sender<T> {
+    send: usize,
+    capacity: usize,
+    shared: Arc<Shared<T>>,
+}
+
+pub struct Receiver<T> {
+    recv: usize,
+    capacity: usize,
+    shared: Arc<Shared<T>>,
+}
+
+pub struct SendGuard<'a, T> {
+    sender: &'a mut Sender<T>,
+    range: Range<usize>,
+}
+
+pub struct RecvGuard<'a, T> {
+    receiver: &'a mut Receiver<T>,
+    range: Range<usize>,
+}
+
+struct Shared<T> {
+    send: crossbeam::utils::CachePadded<AtomicUsize>,
+    recv: crossbeam::utils::CachePadded<AtomicUsize>,
+    data: Box<[UnsafeCell<T>]>,
+}
+
+unsafe impl<T> Send for Shared<T> where T: Send {}
+unsafe impl<T> Sync for Shared<T> {}
+
+#[derive(Debug)]
+pub struct ChannelClosed;
+
+pub fn channel<T>(capacity: usize, mut default: impl FnMut() -> T) -> (Sender<T>, Receiver<T>) {
+    assert!(capacity.is_power_of_two());
+    let mut data = Vec::with_capacity(capacity);
+    for _ in 0..capacity {
+        data.push(UnsafeCell::new(default()));
+    }
+    let data = data.into_boxed_slice();
+    let shared = Arc::new(Shared {
+        send: crossbeam::utils::CachePadded::new(AtomicUsize::new(0)),
+        recv: crossbeam::utils::CachePadded::new(AtomicUsize::new(0)),
+        data,
+    });
+    let sender = Sender {
+        send: 0,
+        capacity,
+        shared: Arc::clone(&shared),
+    };
+    let receiver = Receiver {
+        recv: 0,
+        capacity,
+        shared: Arc::clone(&shared),
+    };
+    (sender, receiver)
+}
+
+impl<T> Sender<T> {
+    pub fn try_send(&mut self) -> Result<SendGuard<'_, T>, ChannelClosed> {
+        if Arc::strong_count(&self.shared) == 1 {
+            return Err(ChannelClosed);
+        }
+        let recv = self.shared.recv.load(Ordering::Acquire);
+        let used = self.send.wrapping_sub(recv);
+        let free = self.capacity - used;
+        let start = self.send & (self.capacity - 1);
+        let end = (start + free).min(self.capacity);
+        let range = start..end;
+        Ok(SendGuard {
+            sender: self,
+            range,
+        })
+    }
+}
+
+impl<T> Receiver<T> {
+    pub fn try_recv(&mut self) -> Result<RecvGuard<'_, T>, ChannelClosed> {
+        if Arc::strong_count(&self.shared) == 1 {
+            return Err(ChannelClosed);
+        }
+        let send = self.shared.send.load(Ordering::Acquire);
+        let used = send.wrapping_sub(self.recv);
+        let start = self.recv & (self.capacity - 1);
+        let end = (start + used).min(self.capacity);
+        let range = start..end;
+        Ok(RecvGuard {
+            receiver: self,
+            range,
+        })
+    }
+}
+
+impl<T> SendGuard<'_, T> {
+    pub fn commit(self) {
+        self.sender.send = self
+            .sender
+            .send
+            .wrapping_add(self.range.end - self.range.start);
+        self.sender
+            .shared
+            .send
+            .store(self.sender.send, Ordering::Release);
+    }
+}
+
+impl<T> RecvGuard<'_, T> {
+    pub fn decommit(self) {
+        self.receiver.recv = self
+            .receiver
+            .recv
+            .wrapping_add(self.range.end - self.range.start);
+        self.receiver
+            .shared
+            .recv
+            .store(self.receiver.recv, Ordering::Release);
+    }
+}
+
+impl<T> Deref for SendGuard<'_, T> {
+    type Target = [T];
+    fn deref(&self) -> &Self::Target {
+        unsafe {
+            let pointer = self.sender.shared.data.as_ptr().add(self.range.start);
+            let pointer = UnsafeCell::raw_get(pointer);
+            std::slice::from_raw_parts(pointer, self.range.end - self.range.start)
+        }
+    }
+}
+
+impl<T> DerefMut for SendGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe {
+            let pointer = self.sender.shared.data.as_ptr().add(self.range.start);
+            let pointer = UnsafeCell::raw_get(pointer);
+            std::slice::from_raw_parts_mut(pointer, self.range.end - self.range.start)
+        }
+    }
+}
+
+impl<T> Deref for RecvGuard<'_, T> {
+    type Target = [T];
+    fn deref(&self) -> &Self::Target {
+        unsafe {
+            let pointer = self.receiver.shared.data.as_ptr().add(self.range.start);
+            let pointer = UnsafeCell::raw_get(pointer);
+            std::slice::from_raw_parts(pointer, self.range.end - self.range.start)
+        }
+    }
+}
+
+impl<T> DerefMut for RecvGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe {
+            let pointer = self.receiver.shared.data.as_ptr().add(self.range.start);
+            let pointer = UnsafeCell::raw_get(pointer);
+            std::slice::from_raw_parts_mut(pointer, self.range.end - self.range.start)
+        }
+    }
+}
+
+impl<T> SendGuard<'_, T> {
+    pub fn truncate(&mut self, new_length: usize) {
+        let length = (self.range.end - self.range.start).min(new_length);
+        self.range.end = self.range.start + length;
+    }
+}
+
+impl<T> RecvGuard<'_, T> {
+    pub fn truncate(&mut self, new_length: usize) {
+        let length = (self.range.end - self.range.start).min(new_length);
+        self.range.end = self.range.start + length;
+    }
+}
